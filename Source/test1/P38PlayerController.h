b11fc941c412454d28488ef0d55793e4bdeb2007
JourneyKiim <journeykiiim@gmail.com>
2023-04-25 12:55:59 +0900

    statr

---
+++ Source/test1/P38PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "P38PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST1_API AP38PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
